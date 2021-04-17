5528f3e1e0185607b6d1ab9d18dbce9af751c52a
John Thompson <john@springframework.guru>
2021-04-17 14:54:06 -0400

    initial project add

---
+++ src/test/java/guru/springframework/streamingstockquoteservice/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.streamingstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
